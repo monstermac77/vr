778a93e3429f1d95c881d3c732d28ce1514ac801
Joe Puccio <josephpuccio@gmail.com>
2021-01-03 09:16:14 -0500

    added to config.

--- config.bat
+++ config.bat
@@ -2,13 +2,16 @@
 :: This script was created for users of MixedVR (see the /r/MixedVR subreddit) by monstermac77
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: specify the MAC addresses of your lighthouses here, separated by a space
+:: specify the MAC addresses of your lighthouses (a.k.a basestations) here, separated by a space
 :: If you have an Android device, you can downnload the "Lighthouse Power Management" app to find the MAC addresses: https://play.google.com/store/apps/details?id=com.jeroen1602.lighthouse_pm
 :: Alternatively, you can:
 :: 1. Open up "Command Prompt"
 :: 2. Navigate to this directory, e.g. `cd C:\Users\Joe Puccio\Dropbox\big-bro-peep\vr\bin\`
 :: 3. Run `lighthouse-v2-manager.exe discover`
 set lighthouseMACAddressList=FE:D0:49:F5:78:D6 E2:81:7F:AC:2B:ED
+
+:: specify what version of lighthouses (a.k.a basestations) you're using, 2.0 or 1.0
+set lighthouseVersion=2.0
 
 :: maxWaitTimeForRoomSetup is how long (in seconds) you ever would expect it to take for the 
 :: SteamVR Room Setup to open after a launch of SteamVR. This is essentially a timeout value.
