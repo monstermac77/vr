5e7d17c6a2d1d890f6a0b7ae181b7bee6f21bf29
Joe Puccio <josephpuccio@gmail.com>
2021-02-06 22:58:54 -0500

    just wanted to briefly test that we still need the vbs hack.

--- bin/mixedvr-manager-startup-wrapper.bat
+++ bin/mixedvr-manager-startup-wrapper.bat
@@ -2,6 +2,7 @@
 
 :: NOTE: If you want to launch MixedVR-Manager manually in a terminal window, please run "mixedvr-manager.bat" as admin
 :: this file should only be run by the scheduled task in elevated permissions
+:: Another note: have confirmed that we still need to launch through vbs to make the program run headlessly
 
 :: file path for MixedVR-Manager from argument passed in by scheduled task
 set FilePath=%1
