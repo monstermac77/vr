88da3ccbe52f74c32d3d15e4088392f37b963658
Joe Puccio <josephpuccio@gmail.com>
2021-02-06 21:42:31 -0500

    realized this is why a lot of people think mixedvr manager doesn't work or ruins their stuff.

--- uninstall-mixedvr-manager.bat
+++ uninstall-mixedvr-manager.bat
@@ -9,6 +9,8 @@ taskkill /f /t /fi "Windowtitle eq MixedVR Manager"
 :: delete scheduled task
 schtasks /Delete /TN "VR\Mixed VR Manager" /F
 
+:: TODO undo any processes here that people would expect to be undone, like re-enable the G2 HMD
+
 echo MixedVR-Manager has been removed
 
 pause
